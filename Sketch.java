a8b41acf7af3d4ce19b25818b41fd5f0453827c7
angel <angel@localhost>
2021-12-10 13:40:46 -0500

    Space

--- Sketch.java
+++ Sketch.java
@@ -2,26 +2,15 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
   public void settings() {
-	// put your size call here
+
     size(400, 400);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
     background(0);
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
 	  
 	  
@@ -87,6 +76,5 @@ public class Sketch extends PApplet {
 	  
 	  
   }
-  
-  // define other methods down here.
+
 }
